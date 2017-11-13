54cada6ffb25637eba2cea046bf29e4f5355ff5b
Raj Singh <raj@foreversingh.com>
2017-11-13 17:16:22 -0600

    second module from learn.chef.io complete

---
+++ module_2/learn-chef/cookbooks/learn_chef_apache2/metadata.rb
@@ -0,0 +1,9 @@
+name 'learn_chef_apache2'
+maintainer 'Raj Singh'
+maintainer_email 'raj@foreversingh.com'
+license 'all_rights'
+description 'Installs/Configures learn_chef_apache2'
+long_description 'Installs/Configures learn_chef_apache2'
+version '0.3.2'
+issues_url 'https://github.com/learn-chef/learn_chef_apache2/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/learn_chef_apache2' if respond_to?(:source_url)
