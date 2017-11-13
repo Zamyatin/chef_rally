a298fd9ab29b7a3a1ecdf23161f9d2ef0f9c8775
Raj Singh <raj@foreversingh.com>
2017-11-13 17:15:24 -0600

    first local module from learn.chef.io complete

---
+++ module_1/chef-repo/cookbooks/learn_chef_apache2/metadata.rb
@@ -0,0 +1,22 @@
+name 'learn_chef_apache2'
+maintainer 'Raj Singh'
+maintainer_email 'raj@foreversingh.com'
+license 'All Rights Reserved'
+description 'Installs/Configures learn_chef_apache2'
+long_description 'Installs/Configures learn_chef_apache2'
+version '0.2.1'
+
+chef_version '>= 12.1' if respond_to?(:chef_version)
+issues_url 'https://github.com/learn-chef/learn_chef_apache2/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/learn-chef/learn_chef_apache2' if respond_to?(:source_url)
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/learn_chef_apache2/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/learn_chef_apache2'
---
+++ module_1/chef-repo/hello.rb
@@ -0,0 +1,3 @@
+file '/tmp/motd' do
+  content "Hello World! Today's date is #{Time.now}"
+end
